e0fcf52f0568b4a284d01b65fd58edbf4c151e1a
trrito <63961996+trrito@users.noreply.github.com>
2020-04-23 22:58:04 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -214,7 +214,7 @@ $.get("js/profile.json",
 		}
 		profile = data;
 		var pInfo = profile.personalInfo;
-		$('title').html(pInfo.nick+'|Portfolio');
+		$('title').html(Portfolio');
 		$('#name').html(pInfo.fname+' '+pInfo.lname+'<sub>&lt'+pInfo.nick+'/&gt</sub>');
 		$('#image img').attr('src','img/'+pInfo.myimg);
 		$('#contact').html(pInfo.mob+'</br>'+pInfo.email);
